128249562bcbeda134fb05ccfe0408affb3e1481
Michael Hanuschik <mammoth@Michaels-MacBook-Pro.local>
2026-03-04 09:42:10 -0800

    update Mar 04 09:42AM

--- netlify/functions/cron.mjs
+++ netlify/functions/cron.mjs
@@ -5,11 +5,11 @@
 
 import { getStore } from "@netlify/blobs";
 
-const MAX_VERIFY_PER_RUN = 10;
+const MAX_VERIFY_PER_RUN = 5;
 const CLAUDE_TIMEOUT_MS  = 15000;
 const BLOB_TIMEOUT_MS    = 5000;
 const RSS_TIMEOUT_MS     = 8000;
-const ITEMS_PER_SOURCE   = 3;
+const ITEMS_PER_SOURCE   = 15;
 
 const SOURCES = {
   // NEWS
